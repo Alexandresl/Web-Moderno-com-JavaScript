c9564ca7c18ae361b5f718b0014c42f3c1b7001d
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-18 15:23:38 -0300

    Usando a Estrutura FOR/IN

---
+++ controle/for2.js
@@ -0,0 +1,22 @@
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for (let i in notas) {
+
+    console.log(`indice ${i} => nota ${notas[i]}`)
+
+}
+
+const pessoa = {
+
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 29,
+    peso: 64
+
+}
+
+for (let atributo in pessoa) {
+
+    console.log(`Atributo ${atributo} => ${pessoa[atributo]}`)
+
+}
