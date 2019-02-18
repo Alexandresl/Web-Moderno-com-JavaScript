66a68f1754e06075e710a1924d6d21e10c13625b
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-18 12:53:56 -0300

    180220191253

---
+++ controle/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+function soBoaNoticia(nota) {
+
+    if (nota >= 7) {
+
+        console.log('Aprovado com ' + nota)
+
+    }
+
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
---
+++ fundamentos/erro.js
@@ -0,0 +1,38 @@
+function tratarErroELancar(erro) {
+
+    // throw new Error('...')
+    // throw 10
+    // throw false
+    // throw 'mensagem'
+    throw {
+
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date()
+
+    }
+
+}
+
+function imprimirNomeGritado(obj) {
+
+    try {
+
+        console.log(obj.name.toUpperCase() + '!!!')
+
+    } catch(e) {
+
+        tratarErroELancar(e)
+
+    } finally {
+
+        console.log('final')
+
+    }
+
+}
+
+
+const obj = { nome: 'Roberto'}
+
+imprimirNomeGritado(obj)
