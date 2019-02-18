34bed812eb2c915ef6e3a96de2006bedadc36f7b
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-18 14:59:49 -0300

    Usando a Estrutura DO/WHILE

---
+++ controle/doWhile,.js
@@ -0,0 +1,17 @@
+function getInteiroAleatorioEntre(min, max) {
+
+    const valor = Math.random() * (max - min) + min
+
+    return Math.floor(valor)
+
+}
+
+let opcao = -1
+
+do {
+
+    opcao = getInteiroAleatorioEntre(-1, 10)
+
+    console.log(`A opção escolhida foi ${opcao}`)
+
+} while (opcao !== -1)
