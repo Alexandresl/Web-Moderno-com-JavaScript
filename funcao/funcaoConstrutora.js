91d1b4fec3d4ad4c7e2e636c9dbcfa72bdcf9b04
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-20 17:44:50 -0300

    Funções Construtoras

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,39 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // método público
+    this.acelerar = function () {
+
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+
+            velocidadeAtual += delta
+
+        } else {
+
+            velocidadeAtual = velocidadeMaxima
+
+        }
+
+    }
+
+    this.getVelocidadeAtual = function () {
+
+        return velocidadeAtual
+
+    }
+
+}
+
+const uno = new Carro
+
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
