52ba1c6a50dc2761bc4ab65e0d71b13e4bbc896e
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-23 00:10:03 -0300

    86.	Introdução à OO

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// Código não executável
+
+// Procedural
+
+function processamento (valor1, valor2, valor3) {
+
+    
+
+}
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // O foco passou a ser o objeto
