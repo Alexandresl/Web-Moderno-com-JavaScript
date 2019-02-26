861450aab06b465cfd4157f5b5a3be68ee82cc28
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-25 21:44:13 -0300

    105.	Simulando Array com Objeto

---
+++ array/simulandoArray.js
@@ -0,0 +1,18 @@
+const quaseArray = { 
+    0: 'Rafael', 
+    1: 'Ana',
+    2: 'Bia'
+}
+
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() {
+        return Object.values(this)
+    },
+    enumerable: false
+})
+
+console.log(quaseArray[0])
+
+const meuArray = [ 'Rafael', 'Ana', 'Bia' ]
+
+console.log(quaseArray.toString(), meuArray)
