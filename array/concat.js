76cd802f4bf1ba4b0cd96735669ded57b0ec4284
Alexandre Lima <limaalexguitar@gmail.com>
2019-03-01 23:15:15 -0300

    117.	Concat

---
+++ array/concat.js
@@ -0,0 +1,9 @@
+const filhas = ['Valeskah', 'Cialena']
+
+const filhos = ['Uoxiton', 'Uesclei']
+
+const todos = filhas.concat(filhos, 'fulano')
+
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7 ]]))
