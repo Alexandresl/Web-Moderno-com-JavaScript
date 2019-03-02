7a16a24f4e505d379f5f9107d00763cf3de87056
Alexandre Lima <limaalexguitar@gmail.com>
2019-03-02 19:16:31 -0300

    124.	Arquivo package.json

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,7 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+})
