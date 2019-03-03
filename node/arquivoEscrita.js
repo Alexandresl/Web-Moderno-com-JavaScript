5fef5c6be213856ba0b32577b636ada42131a363
Alexandre Lima <limaalexguitar@gmail.com>
2019-03-03 08:55:39 -0300

    132.	Escrevendo Arquivos

---
+++ node/arquivoEscrita.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const produto = {
+    nome: 'Celular',
+    preco: 1249.99,
+    desconto: 0.15
+}
+
+fs.writeFile(__dirname + 'arquivoGerado.json', JSON.stringify(produto), err => {
+
+    console.log(err || 'Arquivo salvo!')
+
+})
