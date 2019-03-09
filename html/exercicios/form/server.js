3c687cf9ba5c5dac8d7a243d6707b254da149d35
Alexandre Lima <limaalexguitar@gmail.com>
2019-03-09 17:59:59 -0300

    179.	Formulário #01

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3003)
