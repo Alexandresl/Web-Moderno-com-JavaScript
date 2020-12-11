0d60ea6ce6325cbd7819a287f96cafac56d5b82c
Alexandre Lima <limaalexguitar@gmail.com>
2020-12-10 22:19:57 -0300

    36. Entendendo o Hoisting

--- Fundamentos/hoisting.js
+++ Fundamentos/hoisting.js
@@ -1,3 +1,9 @@
 console.log('a = ', a); // a =  undefined
 var a = 2; // a atribuição ocorre aqui!
 console.log('a = ', a); // a =  2
+
+// Na prática, o que é executado é
+var a2;
+console.log('a2 = ', a2); // a2 =  undefined
+a2 = 2; // a atribuição ocorre aqui!
+console.log('a2 = ', a2); // a2 =  2
